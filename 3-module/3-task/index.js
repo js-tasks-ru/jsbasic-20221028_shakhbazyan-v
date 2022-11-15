68895b69fc2a52e7b2ace8503e80f0518d272032
BarberiVan <shakhbazyan.v@yandex.ru>
2022-11-15 13:54:20 +0300

    3 module

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,8 @@
 function camelize(str) {
-  // ваш код...
+  return str
+    .split("-")
+    .map(
+      function(word, index) {
+        return index == 0 ? word : word[0].toUpperCase() + word.slice(1)})
+    .join('');
 }
